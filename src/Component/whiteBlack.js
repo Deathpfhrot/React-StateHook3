1c8073bfa6a4df8f928c2b901e4b8af4193a74f9
SuperCoder <supercoder@AirvonSperCoder.fritz.box>
2022-03-07 14:28:15 +0100

    react statehook3

---
+++ src/Component/whiteBlack.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+import './whiteBlack.css'
+
+const White = () => {
+    const [isWhite, setWhite] = useState(true)
+
+    const white = () => setWhite(!isWhite) // toggle zwischen true / false
+
+    return(
+        <div className= {`whiteItem ${isWhite ? "white" : "black"}`}>
+            <h1>{isWhite ? "White" : "Black"}</h1>
+            <button onClick={white}>Change to: {isWhite ? "Black" : "White"}</button>
+        </div>
+    )
+}
+
+export default White
